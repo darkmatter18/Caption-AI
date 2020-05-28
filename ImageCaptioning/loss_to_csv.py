6e646d94a27c37fd16eb926e38b87ee5f7dd3331
Arkadip <in2arkadipb13@gmail.com>
2020-05-28 12:25:54 +0000

    feat: loss stats added - loss.csv contains the losses - loss_to_csv file compiles loss

--- ImageCaptioning/loss_to_csv.py
+++ ImageCaptioning/loss_to_csv.py
@@ -4,11 +4,12 @@ with open('./training_log.txt', 'r') as t:
     s = t.read()
 
 with open('./data.csv', 'w+') as f:
+    f.write("epoch, train_loss, test_loss \n")
     for idx, x in enumerate(s.split('\n')):
 
-        epoch = re.findall("(?<=Epoch \[).*(?=\/3\],)", x)[0]
+        epoch = re.findall("(?<=Epoch \[).*(?=\/5\],)", x)[0]
         batch = re.findall("(?<=, Step \[).*(?=\/3236\],)", x)[0]
-        loss = re.findall("(?<=Loss: ).*(?=, Perplexity)",x)[0]
-
-        # f.write(epoch + "," + batch + "," + loss + "\n")
-        f.write(str(idx + 1) + "," + loss + "\n")
+        train_loss = re.findall("(?<=Loss: ).*(?=, Perplexity)",x)[0]
+        test_loss = re.findall("(?<=, Valid Loss: ).*(?=)", x)[0]
+        
+        f.write(f"{epoch}, {train_loss}, {test_loss}\n")
