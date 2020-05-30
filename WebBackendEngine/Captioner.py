2d355e6c82662186ae0154b256ce7f15976ba11b
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-05-30 17:05:52 +0530

    feat: Captioner.py hidden and vocab
    - init_hidden function added
    - vocab added
    - Model hyper param added
    
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- WebBackendEngine/Captioner.py
+++ WebBackendEngine/Captioner.py
@@ -1,14 +1,27 @@
 import os
 import torch
+import pickle
 from torchvision.transforms import Compose, CenterCrop, ToTensor, Normalize
 
 
 class Captioner:
     def __init__(self, ):
+        # Model paths
         models_path = os.path.join(os.getcwd(), 'models')
+        utils_dir = os.path.join(os.getcwd(), 'utils')
         encoder_path = os.path.join(models_path, "encoder.pt")
         decoder_path = os.path.join(models_path, "decoder.pt")
-        print(encoder_path)
+
+        # Set Vocab
+        with open(os.path.join(utils_dir, 'vocab.pkl'), 'rb') as f:
+            vocab = pickle.load(f)
+            self.word2idx = vocab.word2idx
+            self.idx2word = vocab.idx2word
+
+        # Model Hyper parameters
+        self.device = torch.device("cpu")
+        self.hidden_size = 512
+
         print("Captioner Starting..")
         self.transform = Compose([
             CenterCrop(224),
@@ -21,6 +34,10 @@ class Captioner:
         self.encoder = torch.jit.load(encoder_path)
         self.decoder = torch.jit.load(decoder_path)
         print("Model Setting Done...")
+
+    def init_hidden(self, batch_size):
+        return (torch.zeros(3, batch_size, self.hidden_size, device=self.device),
+                torch.zeros(3, batch_size, self.hidden_size, device=self.device))
 
     def preprocessing(self, image):
         imageT = self.transform(image.resize((224, 224)))
