f6029cb7e85ecf07ca02fa67e502beb50634e528
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-02 18:03:12 +0530

    Development (#2)
    * [BACKEND] useless filename removed
    
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>
    
    * [FRONTEND] file check before submitting
    
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>
    
    * [FRONTEND] upload progress added
    
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/Components/HomeComponent/index.js
+++ frontendui/src/Components/HomeComponent/index.js
@@ -6,34 +6,49 @@ import { useHistory } from 'react-router-dom';
 const HomeComponent = () => {
     const history = useHistory()
     const [file, setfile] = useState(null);
+    const [progress, setprogress] = useState(null);
 
     const uploadApi = async (data) => {
-        try {
-            const res = await axios.post('/api/analyze', data, {
-                headers: {
-                    'Content-Type': "multipart/form-data"
-                }
-            });
-            history.push('/result', {res: res.data});
-        } catch (e) {
-            console.log(e);
-        }   
+        if (file === null) {
+            alert("Select a file before Submitting");
+        }
+        else {
+            try {
+                const res = await axios.post('/api/analyze', data, {
+                    headers: {
+                        'Content-Type': "multipart/form-data"
+                    },
+                    onUploadProgress: (progressEvent) => {
+                        const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                        setprogress(percentCompleted);
+                    }
+                });
+                history.push('/result', { res: res.data });
+            } catch (e) {
+                console.log(e);
+            }
+        }
     }
 
     const upload = (e) => {
         e.preventDefault();
         const formData = new FormData();
         formData.append("file", file);
-        
+
         uploadApi(formData);
     }
     return (
-        <div>
-            <form>
-                <input type="file" name="file" onChange={e => setfile(e.target.files[0])} />
-                <button onClick={upload}>Make Caption</button>
-            </form>
-        </div>
+        <React.Fragment>
+            <div>
+                <form>
+                    <input type="file" name="file" onChange={e => setfile(e.target.files[0])} />
+                    <button onClick={upload}>Make Caption</button>
+                </form>
+            </div>
+            <div>
+                <span>{progress !== null ? `Uploading: ${progress}%` : <React.Fragment />}</span>
+            </div>
+        </React.Fragment>
     )
 }
 
