d15fac19c6d96eaeabb1c58867833567c8b0418d
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-02 17:40:38 +0530

    [FRONTEND] upload progress added
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/Components/HomeComponent/index.js
+++ frontendui/src/Components/HomeComponent/index.js
@@ -6,9 +6,10 @@ import { useHistory } from 'react-router-dom';
 const HomeComponent = () => {
     const history = useHistory()
     const [file, setfile] = useState(null);
+    const [progress, setprogress] = useState(null);
 
     const uploadApi = async (data) => {
-        if(file === null) {
+        if (file === null) {
             alert("Select a file before Submitting");
         }
         else {
@@ -16,29 +17,38 @@ const HomeComponent = () => {
                 const res = await axios.post('/api/analyze', data, {
                     headers: {
                         'Content-Type': "multipart/form-data"
+                    },
+                    onUploadProgress: (progressEvent) => {
+                        const percentCompleted = Math.round((progressEvent.loaded * 100) / progressEvent.total);
+                        setprogress(percentCompleted);
                     }
                 });
-                history.push('/result', {res: res.data});
+                history.push('/result', { res: res.data });
             } catch (e) {
                 console.log(e);
             }
-        }  
+        }
     }
 
     const upload = (e) => {
         e.preventDefault();
         const formData = new FormData();
         formData.append("file", file);
-        
+
         uploadApi(formData);
     }
     return (
-        <div>
-            <form>
-                <input type="file" name="file" onChange={e => setfile(e.target.files[0])} />
-                <button onClick={upload}>Make Caption</button>
-            </form>
-        </div>
+        <React.Fragment>
+            <div>
+                <form>
+                    <input type="file" name="file" onChange={e => setfile(e.target.files[0])} />
+                    <button onClick={upload}>Make Caption</button>
+                </form>
+            </div>
+            <div>
+                <span>{progress !== null ? `Uploading: ${progress}%` : <React.Fragment />}</span>
+            </div>
+        </React.Fragment>
     )
 }
 
