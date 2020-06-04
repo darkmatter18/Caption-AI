905d68eb37fa006aba065eb6a9c4f13365856c0b
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-04 14:20:52 +0530

    feat: Added a good header
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/App.js
+++ frontendui/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { CssBaseline } from '@material-ui/core';
 
 import Routes from './routes';
+import HeaderComponent from './Components/HeaderComponent';
 
 function App() {
   return (
-    <Routes />
+    <React.Fragment>
+      <CssBaseline />
+      <HeaderComponent/>
+      <Routes />
+    </React.Fragment>
   );
 }
 
---
+++ frontendui/src/Components/HeaderComponent/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
+import { blue } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+    appBar: {
+        paddingTop: '0.5rem',
+        paddingBottom: '0.5rem',
+        borderBottom: `1px solid ${theme.palette.divider}`,
+        color: blue.A700,
+        textShadow: `0px 0px 10px ${blue.A100}`
+    },
+    toolbar: {
+        flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+        flexGrow: 1,
+        fontFamily: "'DM Mono', monospace;"
+    },
+    link: {
+        margin: theme.spacing(1, 1.5),
+    }
+}));
+
+const HeaderComponent = () => {
+    const classes = useStyles();
+    return (
+        <React.Fragment>
+            <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+                <Toolbar className={classes.toolbar}>
+                    <Typography variant="h2" color="inherit" noWrap className={classes.toolbarTitle} align="center">
+                        Caption - AI
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </React.Fragment>
+    )
+}
+
+export default HeaderComponent;
