cca5e6679a25eaf721d24699edbbd5deba0d5045
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-04 19:32:54 +0530

    feat: custom theme added
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/App.js
+++ frontendui/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, createMuiTheme, ThemeProvider } from '@material-ui/core';
 
 import Routes from './routes';
 import HeaderComponent from './Components/HeaderComponent';
+import { blue } from '@material-ui/core/colors';
 
-function App() {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue.A400,
+      light: blue.A100,
+      dark: blue.A700
+    }
+  }
+})
+
+const App = () => {
   return (
     <React.Fragment>
-      <CssBaseline />
-      <HeaderComponent/>
-      <Routes />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <HeaderComponent />
+        <Routes />
+      </ThemeProvider>
     </React.Fragment>
   );
 }
