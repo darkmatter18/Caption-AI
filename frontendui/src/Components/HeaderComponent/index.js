156e5ba5ed4566874bfaa6ce459b36a5129c85bb
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-05 10:41:00 +0530

    del: useless lines
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/Components/HeaderComponent/index.js
+++ frontendui/src/Components/HeaderComponent/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
     appBar: {
