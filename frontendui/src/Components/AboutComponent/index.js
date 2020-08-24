b6240ba50d528031b6a5a65e5c3db4b6bff4e700
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-24 13:21:46 +0530

    feat: About Little Change
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/Components/AboutComponent/index.js
+++ frontendui/src/Components/AboutComponent/index.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Container, Typography} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(4)
+    }
+}))
 
 const AboutComponent = () => {
+    const classes = useStyles()
     return (
         <React.Fragment>
-            Heelo
+            <Container className={classes.root}>
+                <Typography variant={"h4"}>
+                    Caption AI
+                </Typography>
+            </Container>
         </React.Fragment>
     )
 }
