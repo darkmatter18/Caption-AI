80486195c4f182706edb94bb94cc28e9e990a476
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-25 19:41:32 +0530

    feat: New Information page
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontendui/src/Components/AboutComponent/index.js
+++ frontendui/src/Components/AboutComponent/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Container, Typography} from "@material-ui/core";
+import {Container, Divider, Grid, Link, Typography} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -13,9 +13,142 @@ const AboutComponent = () => {
     return (
         <React.Fragment>
             <Container className={classes.root}>
-                <Typography variant={"h4"}>
-                    Caption AI
-                </Typography>
+                <Grid container>
+                    <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
+                        <Typography variant={"h4"}>
+                            Caption AI
+                        </Typography>
+                        <Typography variant={"subtitle1"}>
+                            An AI powered website, that can Caption any Image
+                        </Typography>
+                        <Divider/>
+                        <Typography variant={"body1"}>
+                            {"Repository: "}
+                            <Link href={"https://github.com/darkmatter18/Caption-AI"}>
+                                https://github.com/darkmatter18/Caption-AI
+                            </Link>
+                        </Typography>
+                        <br/>
+                        <Typography variant={"h6"}>
+                            Technology Used
+                        </Typography>
+                        <ul>
+                            <li>
+                                <Typography variant={"body1"}>
+                                    <Link href={"https://pytorch.org/"}>
+                                        <b>PyTorch:</b>
+                                    </Link>{" "}
+                                    {"The Heart of the whole AI."}
+                                </Typography>
+                                <Typography variant={"body2"}>
+                                    {"The Deep Learning Model is trained on PyTorch using "}
+                                    <Link href={"http://cocodataset.org/"}>
+                                        <b>COCO Dataset</b>
+                                    </Link>
+                                    . <br/>
+                                    The trained model is served using PyTorch JIT.
+                                </Typography>
+                                <br/>
+                            </li>
+                            <li>
+                                <Typography>
+                                    <Typography variant={"body1"}>
+                                        <Link href={"https://www.starlette.io/"}>
+                                            <b>Starlette:</b>
+                                        </Link> {" "}
+                                        {"ASYNC web framework based on ASGI"}
+                                    </Typography>
+                                    <Typography variant={"body2"}>
+                                        Response Asynchronously the Request.
+                                    </Typography>
+                                    <Typography variant={"body2"}>
+                                        Drives the PyTorch JIT engine to get the response
+                                    </Typography>
+                                </Typography>
+                                <br/>
+                            </li>
+                            <li>
+                                <Typography variant={"body1"}>
+                                    <Link href={"https://reactjs.org/"}>
+                                        <b>React JS:</b>
+                                    </Link>{" "}
+                                    {"JavaScript library for the UI"}
+                                </Typography>
+                                <Typography variant={"body2"}>
+                                    Makes the whole Web App so interactive
+                                </Typography>
+                                <Typography variant={"body2"}>
+                                    The Heart of Frontend
+                                </Typography>
+                                <br/>
+                            </li>
+                            <li>
+                                <Typography variant={"body1"}>
+                                    <Link href={"https://cloud.google.com/appengine"}>
+                                        <b>Google App Engine:</b>
+                                    </Link>{" "}
+                                    {"Hosting"}
+                                </Typography>
+                                <Typography variant={"body2"}>
+                                    Hosting the High Memory consuming Deep Learning Model
+                                </Typography>
+                                <br/>
+                            </li>
+                            <li>
+                                <Typography variant={"body1"}>
+                                    {"Other Technologies are: "}
+                                    <Link href={"https://gunicorn.org/"}>
+                                        <b>Gunicorn</b>
+                                    </Link>{", "}
+                                    <Link href={"https://github.com/features/actions"}>
+                                        <b>Github Actions</b>
+                                    </Link>
+                                </Typography>
+                            </li>
+                        </ul>
+                    </Grid>
+                    <Grid item xl={6} lg={6} md={6} sm={6} xs={12}>
+                        <Typography variant={"h4"}>
+                            Developer
+                        </Typography>
+                        <Typography variant={"subtitle1"}>
+                            The Man behind the Innovation
+                        </Typography>
+                        <Divider/>
+                        <Typography variant={"h5"}>
+                            {"Arkadip Bhattacharya ("}
+                            <Link href={"https://github.com/darkmatter18"}>
+                                @darkmatter18
+                            </Link>{")"}
+                        </Typography><br/>
+                        <Typography variant={"body1"}>
+                            {"Personal Portfolio: "}
+                            <Link href={"https://www.arkadip.co"}>
+                                https://www.arkadip.co
+                            </Link>
+                        </Typography><br/>
+                        <Typography variant={"body1"}>
+                            {"LinkedIn: "}
+                            <Link href={"https://linkedin.com/in/arkadip"}>
+                                https://linkedin.com/in/arkadip
+                            </Link>
+                        </Typography><br/>
+                        <Typography variant={"body1"}>
+                            {"Github: "}
+                            <Link href={"https://github.com/darkmatter18"}>
+                                https://github.com/darkmatter18
+                            </Link>
+                        </Typography><br/>
+                        <Divider/><br/>
+                        <Typography variant={"h6"}>
+                            WE DO NOT STORE ANY DATA
+                        </Typography>
+                        <br/>
+                        <Typography variant={"h4"}>
+                            Thank you
+                        </Typography>
+                    </Grid>
+                </Grid>
             </Container>
         </React.Fragment>
     )
